4366844ec681522ccd875b540a494304fc0c3248
phanikumarkodukulla <157877058+phanikumarkodukulla@users.noreply.github.com>
2025-05-26 22:37:26 +0530

    Update settings.py

--- aibot/aibot/settings.py
+++ aibot/aibot/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s2brij!flv*e)==gtz-14mg-3f37#yroj_-9$7)b58)((qotl7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "foxe-ai.onrender.com",
+]
+
 
 
 # Application definition
